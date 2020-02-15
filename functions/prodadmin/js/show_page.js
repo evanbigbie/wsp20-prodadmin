3f81d65003106807ea06837f09a072014453eb7f
Evan Bigbie <wbigbie@gmail.com>
2020-02-14 18:25:53 -0600

    edit functionality fully working

--- functions/prodadmin/js/show_page.js
+++ functions/prodadmin/js/show_page.js
@@ -41,7 +41,7 @@ async function show_page_secured() {
 
     if (products.length === 0) {
         // using += will allow to keep the navigation menu on the top
-        glPageContent += '<h1>No products in the database</h1>'
+        glPageContent.innerHTML += '<h1>No products in the database</h1>'
         return
     }
 
@@ -69,6 +69,153 @@ async function show_page_secured() {
         `;
     }
 }
+
+// make global in this file so can be used outside of function:
+let cardOriginal
+let imageFile2Update
+
+// edit:
+
+function editProduct(index) {
+    const p = products[index]
+    const card = document.getElementById(p.docId)
+    // save the original HTML
+    cardOriginal = card.innerHTML
+    // copied this HTML code from add_page.js, added ${} values, 
+    // removed error message in case want to keep same image
+    // onclick --> update(), cancel()
+    card.innerHTML = `
+    <div class="form-group">
+        Name: <input class="form-control" type="text" id="name" value="${p.name}"/>
+        <p id="name_error" style="color:red;" />
+    </div>
+    <div class="form-group">
+        Summmary: <br>
+        <textarea class="form-control" id="summary" cols="40" rows="5">${p.summary}</textarea>
+        <p id="summary_error" style="color:red;" />
+    </div>
+    <div class="form-group">
+        Price: <input class="form-control" type="text" id="price" value="${p.price}"/>
+        <p id="price_error" style="color:red;" />
+    </div>
+    Current Image:<br>
+    <img src="${p.image_url}"><br>
+    <div class="form-group">
+        New Image: <input type="file" id="imageButton" value="upload" />
+    </div>
+    <button class="btn btn-danger" type="button" onclick="update(${index})">Update</button>
+    <button class="btn btn-secondary" type="button" onclick="cancel(${index})">Cancel</button>
+    `;
+
+    // new image:
+
+    // add event listener 
+    // (imageButton from 'New Image: ... id="imageButton")
+    const imageButton = document.getElementById('imageButton')
+    imageButton.addEventListener('change', e => {
+        // get file selected by the button
+        imageFile2Update = e.target.files[0]
+    })
+}
+
+// update:
+
+async function update(index) {
+    const p = products[index]
+    // name from: id="name"
+    const newName = document.getElementById('name').value
+    const newSummary = document.getElementById('summary').value
+    const newPrice = document.getElementById('price').value
+
+    // validate new values (don't need to include image in validation)
+    const nameErrorTag = document.getElementById('name_error')
+    const summaryErrorTag = document.getElementById('summary_error')
+    const priceErrorTag = document.getElementById('price_error')
+    // these functions were already written for 'add'
+    nameErrorTag.innerHTML = validate_name(newName)
+    summaryErrorTag.innerHTML = validate_summary(newSummary)
+    priceErrorTag.innerHTML = validate_price(newPrice)
+
+    // if (... 'is set')
+    if (nameErrorTag.innerHTML || summaryErrorTag.innerHTML || priceErrorTag.innerHTML) {
+        return
+    }
+
+    // ready to update the db
+
+    // flag
+    let updated = false
+    // begin with empty object
+    const newInfo = {}
+    // if there is any change
+    if (p.name !== newName) {
+        newInfo.name = newName
+        updated = true
+    }
+    if (p.summary !== newSummary) {
+        newInfo.summary = newSummary
+        updated = true
+    }
+    if (p.price !== newPrice) {
+        // have to nest Number here
+        newInfo.price = Number(Number(newPrice).toFixed(2))
+        updated = true
+    }
+    // if not set
+    if (imageFile2Update) {
+        updated = true
+    }
+    if (!updated) {
+        cancel(index)
+        return
+    }
+
+    // update db:
+
+    try {
+
+        // 1. update the image:
+
+        // if image file is set
+        if (imageFile2Update) {
+            // upload image first
+            const imageRef2del = firebase.storage().ref().child(IMAGE_FOLDER + p.image)
+            await imageRef2del.delete()
+            //store new image (unique name with the current time)
+            const image = Date.now() + imageFile2Update.name
+            const newImageRef = firebase.storage().ref(IMAGE_FOLDER + image)
+            // store actual image
+            const taskSnapshot = await newImageRef.put(imageFile2Update)
+            // get url
+            const image_url = await taskSnapshot.ref.getDownloadURL()
+            // update a new image object
+            newInfo.image = image
+            newInfo.image_url = image_url
+        }
+
+        // 2. update the document in Firestore:
+
+        // only fields that exist will be updated here
+        await firebase.firestore().collection(COLLECTION).doc(p.docId).update(newInfo)
+        // go to Show:
+        window.location.href = '/show'
+    } catch (e) {
+        glPageContent.innerHTML = 'Firestore/Storage update error<br>' + JSON.stringify(e)
+    }
+}
+
+// cancel:
+
+function cancel(index) {
+    // get the reference to the product:
+    const p = products[index]
+    // get the reference to the card
+    const card = document.getElementById(p.docId)
+    // insert copy of original, no change will occur
+    card.innerHTML = cardOriginal
+}
+
+// delete:
 
 // index is provided
 async function deleteProduct(index) {
